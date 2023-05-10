a4af42c94025649f5ab7540df695f52c68cbd41a
kkjspdlqj <kkjspdlqj@naver.com>
2023-05-10 09:57:06 +0900

    TEST

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
-
-print("action test")
-
 def func(x):
     return x + 1
 
 # 테스트 함수
 def test_answer():
-    assert func(3) == 5, '오류입니다.'
+    assert func(4) == 5, '오류입니다.'
+    
+    
+print("action test")
